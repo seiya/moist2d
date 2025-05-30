8cb908e806de333c69c2ea0e8d04e6cba3727453
Seiya Nishizawa <seiya@gfd-dennou.org>
2025-05-30 19:35:12 +0900

    Add exchange_halo test

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,3 +4,13 @@ include("../moist.jl")
 @testset "f2h" begin
     @test f2h(1.0f0, 2.0f0, 3.0f0, 4.0f0) ≈ (1.0f0*4.0f0 + 2.0f0*3.0f0) / 7.0f0
 end
+
+@testset "exchange_halo" begin
+    p = Params(; Nx=4, Nz=2, halo=1, H=200.0f0, dz0=100.0f0)
+    A = zeros(FT, p.Nz, p.ia)
+    interior_vals = reshape(collect(1:p.Nz * p.Nx), p.Nz, p.Nx)
+    A[:, p.is:p.ie] .= interior_vals
+    exchange_halo!(A, p)
+    @test A[:, 1:p.halo] == A[:, (p.ie-p.halo+1):p.ie]
+    @test A[:, (p.ie+1):(p.ie+p.halo)] == A[:, p.is:(p.is+p.halo-1)]
+end
